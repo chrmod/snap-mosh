d29f03254ce5c26c69b7dba02159cf459cd1756c
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-04-30 20:08:45 +0200

    another approach

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -e
+
 docker build -t arm .
-docker run -v $(pwd):$(pwd) -w $(pwd) -t arm sh -c "snapcraft --target-arch=armhf"
+docker run -v $(pwd):$(pwd) -w $(pwd) -t arm sh -c "snapcraft clean; snapcraft --target-arch=armhf"
